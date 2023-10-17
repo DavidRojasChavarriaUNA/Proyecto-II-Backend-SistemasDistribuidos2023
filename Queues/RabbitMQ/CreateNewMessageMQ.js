2b41b1fd2766d48f8a6b0f94702a50b8bd4382fa
David Rojas Chavarria <davidmfrojas@gmail.com>
2023-10-17 01:41:46 -0600

    Cambio
    Cambio

--- Queues/RabbitMQ/CreateNewMessageMQ.js
+++ Queues/RabbitMQ/CreateNewMessageMQ.js
@@ -3,27 +3,20 @@ const {
 } = require('./ParametrosMQ');
 
 exports.CreateNewMessageMQ = (action, id = undefined, data = undefined) => {
-    let request = {};
+    let request = "";
     switch (action) {
         case Actions.Insert: {
-            request.method = Actions.Insert;
-            request.body = data;
+            request = `{"method":"${Actions.Insert}","body":${data}}`;
             break;
         }
-        case Actions.Update: {
-            request.method = Actions.Update;
-            request.id = id;
-            request.body = data;
+        case Actions.Update:
+            request = `{"method":"${Actions.Update}","id":${id},"body":${data}}`;
             break;
-        }
-        case Actions.Delete: {
-            request.method = Actions.Delete;
-            request.id = id;
+        case Actions.Delete:
+            request = `{"method":"${Actions.Delete}","id": ${id} }`;
             break;
-        }
         default:
             throw 'Acción no implementada';
     }
-    const requestJSON = JSON.parse(request);
-    return requestJSON;
+    return request;
 }
